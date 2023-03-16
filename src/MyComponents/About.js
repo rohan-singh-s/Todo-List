a991ba8650fafd1e6b688e457791c886db1710d0
rohan-singh-s <rohansingh81324@gmail.com>
2023-03-16 23:50:59 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>This is an about component</h1>
+        <p>
+        A todo list is a simple tool that helps individuals or teams keep track of tasks or activities they need to complete. It is a list of tasks or goals that need to be accomplished, typically written down in a notebook, digital application, or on a piece of paper. Todo lists can be used for personal or professional purposes, and can be organized by priority, deadline, or category. They are a great way to stay organized, increase productivity, and reduce stress by breaking down larger tasks into smaller, manageable steps. By regularly updating and checking off completed tasks, individuals can feel a sense of accomplishment and stay motivated to tackle their goals.
+        </p>
+      </div>
+  
+    </div>
+  );
+};
